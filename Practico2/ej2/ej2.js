d0b06743490b9ee4ff54f16b7332a9c62272182c
JNZader <javierzader@hotmail.com>
2024-04-25 18:43:35 -0300

    tp2 ej2

--- Practico2/ej2/ej2.js
+++ Practico2/ej2/ej2.js
@@ -1,2 +1,8 @@
 /**2.- Desarrolle un módulo fecha que pueda indicarnos la fecha y hora actual. Cree algún 
 código que requiera el módulo y pruebe su funcionamiento en el navegador.  */
+const fechaActual=()=>{
+    const fecha=new Date();
+    return fecha.toLocaleString();
+}
+
+console.log('Fecha y hora actual: ',fechaActual())

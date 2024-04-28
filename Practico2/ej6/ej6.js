dac6d7f752927080dbc927d0aa505516983e3aeb
JNZader <javierzader@hotmail.com>
2024-04-27 21:28:36 -0300

    tp2 ej6

--- Practico2/ej6/ej6.js
+++ Practico2/ej6/ej6.js
@@ -45,3 +45,54 @@ e.- Un array de personas que al menos tenga 2 hobbies
 f.- Un array con todos los diferentes hobbies que existen en personas 
 g.- Un array de los nombres de personas que sean empleados.
  */
+const personas = [ 
+    { 
+    id: 1, 
+   datos: { 
+    nombre: "Jose", 
+    edad: 39, 
+    empleado: true, 
+    hobbies: ["tenis"], 
+    }, 
+    }, 
+    { 
+    id: 2, 
+    datos: { 
+    nombre: "Manuel", 
+    edad: 23, 
+    empleado: true, 
+    }, 
+    }, 
+    { 
+    id: 3, 
+    datos: { 
+    nombre: "Abigail", 
+    edad: 25, 
+    empleado: false, 
+    hobbies: ["basquet", "tenis", "futbol"], 
+    }, 
+    }, 
+    { 
+    id: 4, 
+    datos: { 
+    nombre: "Fabiana", 
+    edad: 12, 
+    empleado: true, 
+    hobbies: ["tenis", "golf"], 
+    }, 
+    }, 
+   ]; 
+/**A) */
+const persona3=()=>personas.find(persona=>persona.id===3).datos;
+/**B) */
+const todosLosDatos=()=>personas.map(persona=>persona.datos);
+/**C) */
+const personasMas24=()=>personas.filter(persona=>persona.datos.edad>24).map(persona=>persona.datos);
+/**D) */
+const personasPorNombre=()=>personas.sort((a,b)=>a.datos.nombre.localeCompare(b.datos.nombre));
+/**E) */
+const personasCon2Hobbies=()=>personas.filter(persona=>persona.datos.hobbies&&persona.datos.hobbies.length>=2);
+/**F) */
+const difHobbies=()=>[...new Set(personas.flatMap(persona=>persona.datos.hobbies))];
+/**G) */
+const nombresEmpleados=()=>personas.filter(persona=>persona.datos.empleado).map(persona=>persona.datos.nombre);
